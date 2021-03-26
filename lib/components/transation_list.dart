1b57543ef3df5f303027411b2192d8f3b5d51bcb
Dionesio Guerra <dionesiojr@gmail.com>
2021-03-26 18:06:04 -0300

    Implementando ListView

--- lib/components/transation_list.dart
+++ lib/components/transation_list.dart
@@ -11,50 +11,50 @@ class TransationList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          // criando uma lista
-          children: transation.map((transation) {
-            return Card(
-              elevation: 5,
-              color: Colors.white,
-              child: Row(
-                children: [
-                  Container(
-                    child: Text(
-                      'R\$ ' + transation.value.toStringAsFixed(2),
+      // Criando lista de elementos
+      child: ListView.builder(
+        itemCount: this.transation.length,
+        itemBuilder: (ctx, index) {
+          final transationList = this.transation[index];
+
+          return Card(
+            elevation: 5,
+            color: Colors.white,
+            child: Row(
+              children: [
+                Container(
+                  child: Text(
+                    'R\$ ' + transationList.value.toStringAsFixed(2),
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black87),
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black12, width: 2)),
+                  padding: EdgeInsets.all(8),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transationList.titulo,
                       style: TextStyle(
+                          fontSize: 16,
                           fontWeight: FontWeight.bold,
-                          fontSize: 20,
                           color: Colors.black87),
                     ),
-                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black12, width: 2)),
-                    padding: EdgeInsets.all(8),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        transation.titulo,
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black87),
-                      ),
-                      Text(
-                        DateFormat('d MMM y H:m').format(transation.date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            );
-            // exibindo a lista
-          }).toList(),
-        ),
+                    Text(
+                      DateFormat('d MMM y H:m').format(transationList.date),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
